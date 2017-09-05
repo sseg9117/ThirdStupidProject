e9be42651c8a1a09705ae8ab361a6bc18743dce4
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-09-05 09:26:31 -0600

    Initial commit

---
+++ src/stupid/controller/StupidController.java
@@ -0,0 +1,10 @@
+package stupid.controller;
+
+public class StupidController
+{
+	public void start()
+	{
+		System.out.println("My name is Sheradon, I have a skate competition on Saturday");
+	}
+}
+	
